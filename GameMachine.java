12a8c08247fa802e2bf70276e7b0eb831f867e63
MiguelAndreDeLosAngeles <79899064+MiguelAndreDeLosAngeles@users.noreply.github.com>
2021-05-03 23:42:10 +0800

    first commit

---
+++ GameMachine.java
@@ -0,0 +1,23 @@
+package arcade;
+
+public class GameMachine {
+	public static void main(String[]args) {
+		Arcade arcademachine = new Arcade();
+		GamePlayer player = new GamePlayer(10);
+		
+		player.CoinInsert(arcademachine, 0);
+		player.CoinInsert(arcademachine, 0);
+		player.CoinInsert(arcademachine, 0);
+		player.CoinInsert(arcademachine, 0);		
+		player.CoinInsert(arcademachine, 0);	
+		player.CoinInsert(arcademachine, 0);
+		player.CoinInsert(arcademachine, 0);
+		player.CoinInsert(arcademachine, 0);
+		player.CoinInsert(arcademachine, 0);		
+		player.CoinInsert(arcademachine, 0);	
+
+	
+		
+	}
+
+}
