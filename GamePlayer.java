e9072547c667e45b061ebe98a0975be9e3d57fd0
MiguelAndreDeLosAngeles <79899064+MiguelAndreDeLosAngeles@users.noreply.github.com>
2021-05-03 23:43:05 +0800

    first commit

---
+++ GamePlayer.java
@@ -0,0 +1,14 @@
+package arcade;
+
+public class GamePlayer{
+	 int coins;
+	
+	public GamePlayer(int coins) {
+		this.coins = 10;
+	}
+	
+	void CoinInsert(Coins coins, int i) {
+		System.out.println(" 1 Coin Inserted");
+		System.out.println("Game Started");
+	}
+}
